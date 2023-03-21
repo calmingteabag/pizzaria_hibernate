5d6c4c97b7d94853e7a21d8ba733f7b7046735de
badcompwaifu <phasedkamo@gmail.com>
2023-03-21 15:17:12 -0300

    added blueprints to implement later

---
+++ src/main/java/com/example/pizzaria/DAO/RemoveCliente.java
@@ -0,0 +1,7 @@
+package com.example.pizzaria.DAO;
+
+public class RemoveCliente {
+
+    public RemoveCliente() {
+    }
+}
---
+++ src/main/java/com/example/pizzaria/DAO/RemovePedidos.java
@@ -0,0 +1,7 @@
+package com.example.pizzaria.DAO;
+
+public class RemovePedidos {
+
+    public RemovePedidos() {
+    };
+}
