c8a8ac87a65c6ac04507362a70e2a020a120b3f1
badcompwaifu <phasedkamo@gmail.com>
2023-03-21 15:24:06 -0300

    updated method names

--- src/main/java/com/example/pizzaria/DAO/CreateCliente.java
+++ src/main/java/com/example/pizzaria/DAO/CreateCliente.java
@@ -1,7 +1,3 @@
-/* 
- * Autoexplicativo, insere um cliente no banco de dados.
- */
-
 package com.example.pizzaria.DAO;
 
 import com.example.pizzaria.Entities.*;
@@ -9,35 +5,43 @@ import com.example.pizzaria.Utils.HibernateSession;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.SelectionQuery;
 
-public class CreateCliente extends ChecagensBancoDados {
+public class CreateCliente {
 
     public CreateCliente() {
     };
 
-    public String insertClient(Clientes cliente, String tableName, String columnName, String clientName,
-            String clienteSobrenome) {
+    public String createNewCliente(Class<Clientes> clienteClass, String tableName, String columnName,
+            String clientName, String clienteSobrenome) {
 
         Session session = HibernateSession.getSession();
-        if (clientName == "" || clientName.length() < 3) {
 
-            return String.format("O nome é invalido, digite um nome válido");
-
-            // } else if (!super.isDuplicate(tableName, columnName, clientName)) {
+        if (clientName == "" || clientName.length() < 2) {
+            return "O nome é invalido, digite um nome maior que 2 caracteres não nulo.";
+        } else if (clienteSobrenome == "" || clienteSobrenome.length() < 2) {
+            return "O nome é invalido, digite um sobrenome maior que 2 caracteres não nulo.";
         }
+
+        String buscaCliente = String.format("SELECT * FROM %s WHERE %s = :buscaValor", tableName, columnName);
+        SelectionQuery<?> clientQuery = session.createNativeQuery(buscaCliente, clienteClass);
+        clientQuery.setParameter("buscaValor", clientName);
+
         try {
+            Clientes cliente = (Clientes) clientQuery.getSingleResult();
             cliente.setNome(clientName);
             cliente.setSobrenome(clienteSobrenome);
-            Transaction transaction = session.beginTransaction();
             session.merge(cliente);
+
+            Transaction transaction = session.beginTransaction();
             transaction.commit();
+            session.close();
+
+            return String.format("O cliente '%s' update com sucesso.", cliente.getNome());
         } catch (Exception exception) {
+            // handle clientes not existing in db
             return String.format("Foi retornado o erro a seguir: %s", exception.getMessage());
         }
-        return String.format("O cliente '%s' foi inserido corretamente.", clientName);
-        // }
-        // return String.format("O cliente '%s' já existe no banco de dados.",
-        // clientName);
     }
 
-};
+}
