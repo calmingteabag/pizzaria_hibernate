29f91d434a10df509dd7b4f93fca039ffa58c9ee
badcompwaifu <phasedkamo@gmail.com>
2023-03-29 02:48:23 -0300

    deleted files

--- src/main/java/com/example/pizzaria/Interfaces/PedidosProdutosInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.pizzaria.Interfaces;
-
-import com.example.pizzaria.Entities.Pedidos;
-
-public interface PedidosProdutosInterface {
-    public Pedidos getPedido();
-
-    public Object getProduto();
-}
