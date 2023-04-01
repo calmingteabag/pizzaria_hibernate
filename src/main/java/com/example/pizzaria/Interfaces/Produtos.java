a1c6a03f630998f6b2b925f27a9e31ec30538989
badcompwaifu <phasedkamo@gmail.com>
2023-03-31 23:31:06 -0300

    removed unecessary comment

--- src/main/java/com/example/pizzaria/Interfaces/Produtos.java
+++ src/main/java/com/example/pizzaria/Interfaces/Produtos.java
@@ -1,23 +1,3 @@
-/* Por que essa interface existe:
- * 
- * Por que as vezes vc não sabe que tipo de Classe vai chamar. Por exemplo, 
- * vc tem um metodo escrito assim:
- * 
- * public void metodoTest(Pintassilgo passarin) {}
- * 
- * Mas vc não sabe se é a classe Pintassilgo, JoaoBarro, Maritaca, mas sabe que
- * todas elas tem um metodo getNome().
- * 
- * O problema é que se no seu método vc fizer um passarin.getNome() ele vai retornar
- * um erro. Vc pode criar um metodo pra cada passarinho mas é inviável pois podem
- * existir milhoes de aves. Então vc generaliza, fazendo isso:
- * 
- * public void metodoTest(Class<?> passarin) {}
- * 
- * E escreve uma interface, fazendo todas as classes que vc pretende usar implementarem
- * essa interface. Então quando vc fizer um passarin.getNome(), ele vai funcionar e executar
- * o que vc quer.
- */
 package com.example.pizzaria.Interfaces;
 
 public interface Produtos {
