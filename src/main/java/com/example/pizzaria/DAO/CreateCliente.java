9837895b5baea67bc9c741d11c73fc0c703f6745
badcompwaifu <phasedkamo@gmail.com>
2023-03-31 23:33:51 -0300

    was trying to search for something that would be inserted made no sense

--- src/main/java/com/example/pizzaria/DAO/CreateCliente.java
+++ src/main/java/com/example/pizzaria/DAO/CreateCliente.java
@@ -1,11 +1,10 @@
 package com.example.pizzaria.DAO;
 
-import com.example.pizzaria.Entities.*;
+import com.example.pizzaria.Models.*;
 import com.example.pizzaria.Utils.HibernateSession;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.SelectionQuery;
 
 public class CreateCliente {
 
@@ -23,21 +22,15 @@ public class CreateCliente {
             return "O nome é invalido, digite um sobrenome maior que 2 caracteres não nulo.";
         }
 
-        String buscaCliente = String.format("SELECT * FROM %s WHERE %s = :buscaValor", tableName, columnName);
-        SelectionQuery<?> clientQuery = session.createNativeQuery(buscaCliente, clienteClass);
-        clientQuery.setParameter("buscaValor", clientName);
-
         try {
-            Clientes cliente = (Clientes) clientQuery.getSingleResult();
-            cliente.setNome(clientName);
-            cliente.setSobrenome(clienteSobrenome);
+            Clientes cliente = new Clientes(clientName, clienteSobrenome);
             session.merge(cliente);
 
             Transaction transaction = session.beginTransaction();
             transaction.commit();
             session.close();
 
-            return String.format("O cliente '%s' update com sucesso.", cliente.getNome());
+            return String.format("O cliente '%s' criado com sucesso.", cliente.getNome());
         } catch (Exception exception) {
             // handle clientes not existing in db
             return String.format("Foi retornado o erro a seguir: %s", exception.getMessage());
