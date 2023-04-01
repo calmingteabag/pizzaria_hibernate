87e68c71369a6b65dd7a5cd8147ea747e89339c6
badcompwaifu <phasedkamo@gmail.com>
2023-03-31 23:35:42 -0300

    method that executes remove client request to controller

--- src/main/java/com/example/pizzaria/DAO/RemoveCliente.java
+++ src/main/java/com/example/pizzaria/DAO/RemoveCliente.java
@@ -1,7 +1,25 @@
 package com.example.pizzaria.DAO;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.example.pizzaria.Models.Clientes;
+import com.example.pizzaria.Utils.HibernateSession;
+
 public class RemoveCliente {
 
     public RemoveCliente() {
+    }
+
+    public String removeClientById(String idCliente) {
+        Session session = HibernateSession.getSession();
+        Transaction transaction = session.beginTransaction();
+        int intIdCliente = Integer.parseInt(idCliente);
+        Clientes cliente = session.get(Clientes.class, intIdCliente);
+        session.remove(cliente);
+        transaction.commit();
+        session.close();
+
+        return String.format("Cliente id %s removido", idCliente);
     }
 }
